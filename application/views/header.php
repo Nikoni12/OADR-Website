c75852b77eff821738f03c19da4288e42d2ea0a9
Nikoni12 <nikocaranguianlustre@gmail.com>
2022-06-10 10:12:19 +0800

    update

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
     <div class="container d-flex justify-content-between align-items-center">
         <div class = "logo">
             <a class="" href="http://localhost/OADR-Website/User/index" title="OADR">
-                <img style="width:200px; height:100px;" src="<?php echo base_url('assets/img/OADRNOBG.png');?>">
+                <img style="width:100%; height:auto;" src="<?php echo base_url('assets/img/OADRNOBG.png');?>">
             </a>
         </div>
         <nav id="navbar" class="navbar">
