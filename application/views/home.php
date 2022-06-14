5d02935e9ec300fc27f7683bcc1bd0a961605704
Nikoni12 <nikocaranguianlustre@gmail.com>
2022-06-14 16:02:15 +0800

    Update

--- application/views/home.php
+++ application/views/home.php
@@ -35,14 +35,13 @@
 		<main id="main">
 		<div class="adrcontain">
 	  	<div class="sect_post">
-			
 			<div class="sect_img">
 			<iframe class="col-md-12" width="600" height="480" src="https://www.youtube.com/embed/tGFijuVyzyQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</div>
 
 			<div class="sect_info">
 
-				<h1 class ="sect_title">What is Alternative Dispute Resolution (ADR)?</h1>
+				<h2 class ="sect_title">What is Alternative Dispute Resolution (ADR)?</h2>
 				<div class="border"></div>
 				<p class="sect_text">
 					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
