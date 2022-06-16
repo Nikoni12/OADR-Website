87ab1f08f84e5523674773431b7c812d350d8a73
Nikoni12 <nikocaranguianlustre@gmail.com>
2022-06-16 16:39:49 +0800

    update

--- application/views/careerform.php
+++ application/views/careerform.php
@@ -47,7 +47,7 @@
                                 </nav>
                             </div>
                             <div class="col-md-8" style = "border-left:1px solid #031261; padding-left:20px;padding-right:20px;">
-							<div class="newscarhead"><h1><u>Application Form</u></h1><br></div>
+							<div class="newscarhead"><h1>Application Form</h1><br></div>
 							<form>
                     <div class="form-group">
                         <label for="username">Name:</label>
--- application/views/technicalconsultant.php
+++ application/views/technicalconsultant.php
@@ -47,7 +47,7 @@
                                 </nav>
                             </div>
                             <div class="col-md-8" style = "border-left:1px solid #031261; padding-left:20px;padding-right:20px;">
-							<div class="newscarhead"><h1><u>Technical Consultant</u></h1><br></div> 
+							<div class="newscarhead"><h1>Technical Consultant</h1><br></div> 
 							<h4>OFFICE FOR ALTERNATIVE DISPUTE RESOLUTION | NCR</h4>
 							<div id=""><b>Place of Assignment:</b>Compliance and Monitoring Division, Policy, Compliance and Monitoring Service</div>
 							<div id=""><b>Position Title :</b> Administrative Assistant</div>
